63f3cd85d3d17ba25863108d3d8d5c6678ee8103
vishalmighty <69051998+vishalmighty@users.noreply.github.com>
2024-11-28 14:08:14 +0530

    added write to csv pyspark

--- apache spark/basics/write_to_csv.py
+++ apache spark/basics/write_to_csv.py
@@ -1,6 +1,17 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession,dataframe
+
 
 spark = SparkSession.builder \
     .appName('read csv') \
     .config("spark.sql.adaptive.enabled", "false") \
     .getOrCreate()
+
+# print(help(dataframe))
+
+data = [(1,"vish"),(2,"rand1")]
+schema = ['id','name']
+df = spark.createDataFrame(data=data,schema=schema)
+
+df.write.csv(path='/Users/vishal/Documents/GitHub/DATA-ENGINEERING/apache spark/basics/test',header=True) # this will write in multiple files due to parallelism of worker nodes
+
+#mode = overwrite , ignore , append, error

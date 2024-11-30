34df51656ab016468deacff17ca6530acfb75ed5
vishalmighty <69051998+vishalmighty@users.noreply.github.com>
2024-11-30 16:57:56 +0530

    added many func pyspark

--- apache spark/basics/funcs.py
+++ apache spark/basics/funcs.py
@@ -33,3 +33,22 @@ df.filter("age > 18").show()  # String expression
 df.where(df['age'] > 18).show()
 df.where(df.age > 18).show()
 df.where("age > 18").show()  # String expression
+
+#############################
+# distinct - distinct of all columns df.distinct()
+# dropDuplicates - distinct of one or multiple columns df.dropDuplicates(), df.dropDuplicates(['column1'])
+
+#############################
+# sort() & orderBy are same
+# df.sort('id')
+# df.sort(df.id)
+# df.sort(df.id.desc())
+# df.sort(df.name.desc(),df.id.desc())
+
+# df.orderBy('id')
+
+#############################
+# union & unionAll - both are same, union will not work as sql/will not remove duplicates
+# df3 = df1.union(df2)
+#to remove duplicates
+# df3.distinct()
